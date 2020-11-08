241022b046bcbc8673acf9efc8267a9bb147e139
Grégoire Duchêne <gduchene@awhk.org>
2020-11-08 18:07:46 +0000

    darwin/emacs: Use executable-find to find gls

--- dotfiles-darwin/emacs/init.el
+++ dotfiles-darwin/emacs/init.el
@@ -1,5 +1,4 @@
 (setq mac-right-option-modifier 'none)
 
-(let ((gnu-ls "/usr/local/bin/gls"))
-  (when (file-exists-p gnu-ls)
-    (setq insert-directory-program gnu-ls)))
+(when (executable-find "gls")
+  (setq insert-directory-program "gls"))
