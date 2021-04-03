2e0b4bdff5fdf3fb9069424c25528b24c7b68261
Grégoire Duchêne <gduchene@awhk.org>
2021-04-03 18:05:56 +0100

    darwin/bin: Export DOMAIN and HOST in env.sh

--- dotfiles-darwin/bin/env.sh
+++ dotfiles-darwin/bin/env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-for env in UNAME XDG_{{CACHE,CONFIG,DATA}_HOME,RUNTIME_DIR}; do
+for env in DOMAIN HOST UNAME XDG_{{CACHE,CONFIG,DATA}_HOME,RUNTIME_DIR}; do
   launchctl setenv ${env} ${(P)env}
 done
