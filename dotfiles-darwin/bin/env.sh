8c4ca999c2487e32f4bba59ebcb826b6d72d6d13
Grégoire Duchêne <gduchene@awhk.org>
2023-01-15 12:41:54 +0000

    darwin/zsh: Switch back to Homebrew-provided Emacs

--- dotfiles-darwin/bin/env.sh
+++ dotfiles-darwin/bin/env.sh
@@ -7,20 +7,3 @@ for env in DOMAIN HOST UNAME XDG_{{CACHE,CONFIG,DATA}_HOME,RUNTIME_DIR}; do
   echo_log Setting ${env} to “${(P)env}”.
   launchctl setenv ${env} ${(P)env}
 done
-
-# Some environment variables are magic: setting them with launchctl does
-# not seem to work with Emacs. I’m not sure why, but adding those to
-# Info.plist does work.
-local plist=~/Applications/Emacs.app/Contents/Info.plist
-
-if [[ ! -f ${plist} ]]; then
-  echo_log No Info.plist file found at “${plist}”.
-  return
-fi
-
-plutil -insert LSEnvironment -dictionary ${plist} 2>/dev/null >&2 || true
-for env in PATH SSH_AUTH_SOCK; do
-  echo_log Setting LSEnvironment.${env} in Emacs.app to “${(P)env}”.
-  plutil -replace LSEnvironment.${env} -string ${(P)env} ${plist}
-done
-/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -f ${plist:h:h}
