dc95e63e3489f9ec93bf174cd63e8e0943b1353b
Grégoire Duchêne <gduchene@awhk.org>
2023-04-23 13:43:50 +0100

    emacs: Remove old packages

--- emacs/package-list.el
+++ emacs/package-list.el
@@ -1,20 +1,17 @@
 (setq package-selected-packages '(bazel
                                   clang-format
                                   consult
-                                  counsel
                                   diminish
                                   dockerfile-mode
                                   eglot
                                   flycheck
                                   go-mode
                                   google-c-style
-                                  ivy
                                   json-mode
                                   magit
                                   marginalia
                                   markdown-mode
                                   protobuf-mode
-                                  swiper
                                   tuareg
                                   vertico
                                   yaml-mode))
