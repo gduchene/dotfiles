fc8bfbcdadbc9040b4f8d25f82f312a801e593ac
Grégoire Duchêne <gduchene@awhk.org>
2023-07-08 12:16:18 +0100

    darwin/env.sh: Export HOST_SHORT

--- dotfiles-darwin/bin/env.sh
+++ dotfiles-darwin/bin/env.sh
@@ -3,7 +3,8 @@
 zmodload zsh/datetime
 echo_log() { echo $(strftime %+) - $@ }
 
-for env in DOMAIN HOST UNAME XDG_{{CACHE,CONFIG,DATA}_HOME,RUNTIME_DIR}; do
+for env (DOMAIN HOST{,_SHORT} UNAME
+         XDG_{{CACHE,CONFIG,DATA}_HOME,RUNTIME_DIR}); do
   echo_log Setting ${env} to “${(P)env}”.
   launchctl setenv ${env} ${(P)env}
 done
